5afc5d9db2444dfc114c4b6b40523f439e9a1460
nikolai <nikolai@theministry.co>
2022-01-06 11:21:55 +0100

    add background image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,13 @@
 module.exports = {
-  mode: "jit",
-  content: [
-    "./src/**/*.{html,js,svelte,ts, css}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
-  ],
-}
+	mode: 'jit',
+	content: ['./src/**/*.{html,js,svelte,ts, css}'],
+	theme: {
+		extend: {
+			backgroundImage: {
+				rocket: 'url("/img/rocket.png")',
+				launch: 'url("/img/launch.png")'
+			}
+		}
+	},
+	plugins: [require('daisyui')]
+};
