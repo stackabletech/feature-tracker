b987fb2db967dd6989b32b55d84a055d56e65032
nikolai <nikolai@theministry.co>
2022-01-06 11:22:26 +0100

    add bg image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,6 @@
 	import { info } from '$lib/util/alert';
 </script>
 
-<main class="grow grid place-items-center">
+<main class="grow grid place-items-center bg-launch bg-no-repeat bg-bottom">
 	<p on:click={() => info('Hello, World!')}>Hello World.</p>
 </main>
