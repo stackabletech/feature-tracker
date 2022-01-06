894274698472e77045c2d2af9ece380baeffd5c7
nikolai <nikolai@theministry.co>
2022-01-06 12:02:59 +0100

    fix sveltekit prismaclient bug (https://github.com/prisma/prisma/issues/10404)

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,2 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-export const prisma: PrismaClient = new PrismaClient();
+import pkg, { PrismaClient } from "@prisma/client";
+import { dev } from '$app/env';
+
+export const prisma: PrismaClient = dev ? new PrismaClient() : new pkg.PrismaClient();
