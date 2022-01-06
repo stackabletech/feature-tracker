27726fd02c852a8fc640fd200ce957a48f22c86a
nikolai <nikolai@theministry.co>
2022-01-06 11:55:38 +0100

    switch to vercel-specific adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: vercel(),
 		target: '#svelte'
 	}
 };
