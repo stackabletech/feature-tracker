b4937a326915e5c83e29b02c225ac8240268a0bc
nikolai-cc <nikolai@theministry.co>
2022-01-31 09:42:21 +0100

    quirky test for wether items have loaded

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import { categories, products, features, productFeatures } from '$lib/stores.ts';
 	import { info } from '$lib/util/alert';
 </script>
 
 <main class="grow grid place-items-center bg-launch bg-no-repeat bg-bottom">
-	<p on:click={() => info('Hello, World!')}>Hello World.</p>
+	<div class="text-center">
+		<p on:click={() => info('Hello, World!')}>Hello World.</p>
+		<p>
+			Currently {$features.length} features in {$categories.length} categories combine with {$products.length}
+			products into {$productFeatures.length} product-features!
+		</p>
+	</div>
 </main>
