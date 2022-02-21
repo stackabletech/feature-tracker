f30c5b8f83d93cebe81acc330d11888c95d91c4d
nikolai-cc <nikolai@theministry.co>
2022-02-21 18:48:37 +0100

    new cookie handling

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,11 +1,12 @@
+import type { RequestEvent } from "@sveltejs/kit"
 import { parse } from "cookie"
 
-export const handle = async ({ request, resolve }) => {
-	const response = await resolve(request)
+export const handle = async ({ event, resolve }) => {
+	const response = await resolve(event);
 	return response
 }
 
-export const getSession = ({ headers }) => { 
-	const cookies = parse(headers.cookie || "")
-	return { cookies };
+export const getSession = (event: RequestEvent) => { 
+	const cookies = parse(event.request.headers.get('cookie') || "")
+	return cookies;
 }
