31861f52cf79166a194f98b54094272e2493d467
nikolai-cc <nikolai@theministry.co>
2022-03-01 09:43:41 +0100

    initial productfeaturecell

---
+++ src/lib/components/unified/data/ProductFeatureCell.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import type { ProductFeature } from '@prisma/client';
+	export let productFeature: ProductFeature;
+</script>
+
+<td>
+	{productFeature.implementation_status}
+</td>
