7e29f413fdac0abe77ec952fe774bdd447b908f2
nikolai-cc <nikolai@theministry.co>
2022-03-01 09:40:48 +0100

    add nice stats page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
-	import { categories, products, features, productFeatures } from '$lib/stores.ts';
-	import { info } from '$lib/util/alert';
+	import { categories, products, features, productFeatures } from '$lib/stores';
 </script>
 
 <main class="grow grid place-items-center bg-launch bg-no-repeat bg-bottom">
-	<div class="text-center">
-		<p on:click={() => info('Hello, World!')}>Hello World.</p>
-		<p>
-			Currently {$features.length} features in {$categories.length} categories combine with {$products.length}
-			products into {$productFeatures.length} product-features!
-		</p>
+	<div class="shadow md:stats md:stats-horizontal stats-vertical">
+		<div class="stat">
+			<div class="stat-title">Features</div>
+			<div class="stat-value">{$features.length}</div>
+		</div>
+		<div class="stat">
+			<div class="stat-title">Categories</div>
+			<div class="stat-value">{$categories.length}</div>
+		</div>
+		<div class="stat">
+			<div class="stat-title">Products</div>
+			<div class="stat-value">{$products.length}</div>
+		</div>
+		<div class="stat">
+			<div class="stat-title text-secondary">Product Features</div>
+			<div class="stat-value text-secondary">{$productFeatures.length}</div>
+		</div>
 	</div>
 </main>
