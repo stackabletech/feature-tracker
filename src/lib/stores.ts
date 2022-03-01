959b3dde8f2c0334e04e3a3786f45cf9d6025736
nikolai-cc <nikolai@theministry.co>
2022-03-01 09:38:48 +0100

    build up hierarchical categorie store

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -8,27 +8,27 @@ let p: Product[];
 let f: Feature[];
 let pf: ProductFeature[];
 
+// These stores contain the data in a flat array.
 export const categories = writable(c);
 export const products = writable(p);
 export const features = writable(f);
 export const productFeatures = writable(pf);
 
-type HierarchicalCategory = Category & { children?: Category[] }
-// extended category type with array of children
+// HierarchicalCategory extends the category type with an array of children.
+export type HierarchicalCategory = Category & { children?: HierarchicalCategory[] }
 
+// This function recursively builds up a tree of categories.
+const buildTree = (array: HierarchicalCategory[], base: HierarchicalCategory[]) => {
+    return base.reduce((acc, item) => {
+        item.children = array.filter(child => child.parent_id === item.id);
+        item.children = buildTree(array, item.children);
+        return [...acc, item];
+    }, []);
+}
+
+// This store contains an up-to-date tree of categories.
 export const categoryTree = derived(categories, ($categories: Category[]) => {
-    if ($categories.length < 0) {
-        $categories.reduce((tree, category) => {
-            if (category.parent_id === null) {
-                tree.push(category);
-            } else {
-                const parent: HierarchicalCategory = $categories.find((c) => c.id === category.parent_id);
-                if (parent.children === undefined) {
-                    parent.children = [];
-                }
-                parent.children.push(category);
-            }
-            return tree;
-        }, []);
-    } else return [];
+    let base: HierarchicalCategory[] = $categories.filter(c => c.parent_id === null);
+    buildTree($categories, base);
+    return base
 })
