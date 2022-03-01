cf3720ac3c1d83b8ad92b03c4ad83c6a915830c7
nikolai-cc <nikolai@theministry.co>
2022-03-01 09:44:10 +0100

    buildup pages

--- src/routes/edit/unified.svelte
+++ src/routes/edit/unified.svelte
@@ -1,69 +1,24 @@
-<script context="module" lang="ts">
-	export const load = async ({ fetch }) => {
-		const productResponse = await fetch('/api/products.json');
-		const categoryResponse = await fetch('/api/categories.json');
-		const featuresResponse = await fetch('/api/features.json');
-		const productFeaturesResponse = await fetch('/api/product_features.json');
-
-		const products = await productResponse.json();
-		const categories = await categoryResponse.json();
-		const features = await featuresResponse.json();
-		const productFeatures = await productFeaturesResponse.json();
-
-		return {
-			props: {
-				products,
-				categories,
-				features,
-				productFeatures
-			}
-		};
-	};
-</script>
-
 <script lang="ts">
-	export let products;
-	export let categories;
-	export let features;
-	export let productFeatures;
-
-	import { setContext } from 'svelte';
-
-	setContext('products', products);
-	setContext('categories', categories);
-	setContext('features', features);
-	setContext('productFeatures', productFeatures);
-
-	import Cell from '$lib/components/crud/cells/Cell.svelte';
-	import ExpandableRow from '$lib/components/crud/ExpandableRow.svelte';
-
-	const categoryTree = categories.reduce((tree, category) => {
-		if (category.parent_id === null) {
-			tree.push(category);
-		} else {
-			const parent = categories.find((c) => c.id === category.parent_id);
-			if (parent.children === undefined) {
-				parent.children = [];
-			}
-			parent.children.push(category);
-		}
-		return tree;
-	}, []);
-
-	console.log(categoryTree);
+	import { products, categoryTree } from '$lib/stores';
+	import EditableHeader from '$lib/components/unified/data/EditableHeader.svelte';
+	import CategoryRow from '$lib/components/unified/data/CategoryRow.svelte';
+	import { Maximize2Icon, Minimize2Icon } from 'svelte-feather-icons';
 </script>
 
-<table class="table table-zebra">
+<table class="table table-zebra table-compact">
 	<thead>
-		<td colspan="3">Category</td>
-		<td>Feature</td>
-		{#each products as product}
-			<Cell bind:value={product.name} type="name" colspan={4} />
-		{/each}
+		<tr>
+			<th />
+			<th />
+			{#each $products as product}
+				<EditableHeader value={product.name} scope="col" />
+			{/each}
+		</tr>
 	</thead>
 	<tbody>
-		{#each categoryTree as category}
-			<ExpandableRow {category} expanded />
+		{#each $categoryTree as category}
+			<CategoryRow {category} />
 		{/each}
 	</tbody>
+	<tfoot> . </tfoot>
 </table>
---
+++ src/routes/features/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { products, categoryTree } from '$lib/stores';
+	import EditableHeader from '$lib/components/unified/data/EditableHeader.svelte';
+	import FixedCategory from '$lib/components/unified/data/FixedCategory.svelte';
+	import { Maximize2Icon, Minimize2Icon } from 'svelte-feather-icons';
+</script>
+
+<table class="table table-zebra table-compact">
+	<thead>
+		<tr>
+			<th />
+			<th />
+			{#each $products as product}
+				<th>
+					{product.name}
+				</th>
+			{/each}
+		</tr>
+	</thead>
+	<tbody>
+		{#each $categoryTree as category}
+			<FixedCategory {category} />
+		{/each}
+	</tbody>
+	<tfoot> . </tfoot>
+</table>
