cdc3d8fd8ee77f41116d7bb920d1036cbf7b85e4
nikolai-cc <nikolai@theministry.co>
2022-03-15 21:42:47 +0100

    add feature toggle, login Icon, change sidebar buttons

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -30,7 +30,9 @@
 		productFeatures as productFeatureStore
 	} from '$lib/stores';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
-	import { LockIcon, MenuIcon } from 'svelte-feather-icons';
+	import { FeatherIcon, LockIcon, MenuIcon } from 'svelte-feather-icons';
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
 
 	export let products;
 	export let categories;
@@ -45,6 +47,10 @@
 	const toastOptions = {
 		pausable: true
 	};
+
+	onMount(() => {
+		themeChange(false);
+	});
 </script>
 
 <div
@@ -62,6 +68,9 @@
 				<span class="text-lg font-bold">Stackable Feature Tracker</span>
 			</div>
 			<div class="flex-0 mx-2 float-right">
+				<button class="btn btn-square btn-ghost" data-toggle-theme="dark,light" aria-hidden>
+					<FeatherIcon size="24" />
+				</button>
 				<a href="/signin" role="button" class="btn btn-square btn-ghost">
 					<LockIcon size="24" />
 				</a>
@@ -71,10 +80,11 @@
 			<slot />
 		</div>
 	</div>
-	<div class="drawer-side border-r">
-		<label for="sidebar-drawer" class="drawer-overlay" />
-		<aside class="w-40 bg-base bg-opacity-50 backdrop-blur">
-			<div class="navbar sticky top-0 place-content-center z-50 backdrop-blur">
+
+	<div class="drawer-side border-r-[--tab-border-color]">
+		<label for="sidebar-drawer" class="drawer-overlay backdrop-blur" />
+		<aside class="w-48 bg-base bg-opacity-50">
+			<div class="navbar sticky top-0 place-content-center z-50">
 				<a href="/">
 					<div class="bg-white rounded-md w-10 h-10 flex items-center justify-center">
 						<img class="h-8 w-8" src="/img/icon-colour.png" alt="" />
@@ -83,22 +93,22 @@
 			</div>
 			<ul class="menu">
 				<li>
-					<a href="/api">Browse API</a>
+					<a href="/api" class="btn btn-ghost m-2">Browse API</a>
 				</li>
 				<li>
-					<a href="/edit">Edit Data</a>
+					<a href="/edit" class="btn btn-ghost m-2">Edit Data</a>
 				</li>
 				<li>
-					<a href="/features">Feature View</a>
+					<a href="/features" class="btn btn-ghost m-2">Feature View</a>
 				</li>
 				<li>
-					<a href="/products">Product View</a>
+					<a href="/products" class="btn btn-ghost m-2">Product View</a>
 				</li>
 				<li>
-					<a href="/roadmap">Roadmap View</a>
+					<a href="/roadmap" class="btn btn-ghost m-2">Roadmap View</a>
 				</li>
 				<li>
-					<a href="/techtree">Tech Tree View</a>
+					<a href="/techtree" class="btn btn-ghost m-2">Tech Tree View</a>
 				</li>
 			</ul>
 		</aside>
