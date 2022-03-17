f24923877605723ffae320061c14c603fd1ef494
nikolai-cc <nikolai@theministry.co>
2022-03-17 04:37:39 +0100

    prisma import bug

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,19 @@
-import Prisma, * as PrismaAll from '@prisma/client'
+import pkg, { PrismaClient } from '@prisma/client';
+import { dev } from '$app/env';
 
-const pc = Prisma?.PrismaClient || PrismaAll.PrismaClient
-export const prisma = new pc()
+declare global {
+	var _prisma: PrismaClient; // eslint-disable-line
+}
+
+let pr;
+if (dev) {
+	if (!global._prisma) {
+		global._prisma = new PrismaClient();
+	}
+	pr = global._prisma;
+} else {
+	const { PrismaClient } = pkg;
+	pr = new PrismaClient();
+}
+
+export let prisma = pr;
