d13925cf5b13f34f55d6af9ae5262266e292b228
nikolai-cc <nikolai@theministry.co>
2022-03-17 04:44:57 +0100

    revert fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,4 @@
-import pkg, { PrismaClient } from '@prisma/client';
-import { dev } from '$app/env';
+import Prisma, * as PrismaAll from '@prisma/client'
 
-declare global {
-	var _prisma: PrismaClient; // eslint-disable-line
-}
-
-let pr;
-if (dev) {
-	if (!global._prisma) {
-		global._prisma = new PrismaClient();
-	}
-	pr = global._prisma;
-} else {
-	const { PrismaClient } = pkg;
-	pr = new PrismaClient();
-}
-
-export let prisma = pr;
+const pc = Prisma?.PrismaClient || PrismaAll.PrismaClient
+export const prisma = new pc()
