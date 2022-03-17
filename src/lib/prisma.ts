7c30134ca444c0e9ad85e696c88e0f511b74011e
nikolai-cc <nikolai@theministry.co>
2022-03-17 12:05:17 +0100

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,12 +1,15 @@
 // Prisma (workaround until Prisma fully supports ESM)
 // This works in DEV
-import * as Prisma from '@prisma/client'
-import { dev } from '$app/env'
+// import * as Prisma from '@prisma/client'
+// import { dev } from '$app/env'
 
-// This works in PROD
-import { default as ProdPrisma } from '@prisma/client'
+// // This works in PROD
+// import { default as ProdPrisma } from '@prisma/client'
 
-let { PrismaClient } = Prisma
+// let { PrismaClient } = Prisma
 
-if (!dev) PrismaClient = ProdPrisma.PrismaClient
+// if (!dev) PrismaClient = ProdPrisma.PrismaClient
+// export const prisma = new PrismaClient()
+
+import { PrismaClient } from '@prisma/client'
 export const prisma = new PrismaClient()
