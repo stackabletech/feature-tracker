9ec88868cb04fe0bf2e80350515fdb15e0e850ba
nikolai-cc <nikolai@theministry.co>
2022-03-30 14:21:15 +0100

    infoButton component

---
+++ src/lib/components/ui/InfoButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { InfoIcon } from 'svelte-feather-icons';
+	import Button from './Button.svelte';
+
+	let classes: string = '';
+	export { classes as class };
+</script>
+
+<Button on:click class="btn-outline {classes}" tip="show menu">
+	<InfoIcon size="16" />
+</Button>
