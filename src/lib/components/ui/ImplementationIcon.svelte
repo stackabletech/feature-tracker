241707910aaacea70f3789da3cbd61445b19bdb4
nikolai-cc <nikolai@theministry.co>
2022-03-30 14:21:06 +0100

    add custom classes to ImplementationIcon

--- src/lib/components/ui/ImplementationIcon.svelte
+++ src/lib/components/ui/ImplementationIcon.svelte
@@ -2,22 +2,25 @@
 	import type { ImplementationStatus } from '$lib/prisma';
 	import { ActivityIcon, CalendarIcon, CheckCircleIcon, XCircleIcon } from 'svelte-feather-icons';
 	export let status: ImplementationStatus;
+
+	let classes = '';
+	export { classes as class };
 </script>
 
 {#if status == 'NOT_PLANNED'}
-	<div class="p-1 rounded-full bg-error">
+	<div class="p-1 rounded-full bg-error {classes}">
 		<XCircleIcon size="14" class="text-white" />
 	</div>
 {:else if status == 'PLANNED'}
-	<div class="p-1 rounded-full bg-info">
+	<div class="p-1 rounded-full bg-info {classes}">
 		<CalendarIcon size="14" class="text-white" />
 	</div>
 {:else if status == 'IN_PROGRESS'}
-	<div class="p-1 rounded-full bg-warning">
+	<div class="p-1 rounded-full bg-warning {classes}">
 		<ActivityIcon size="14" class="text-white" />
 	</div>
 {:else if status == 'COMPLETED'}
-	<div class="p-1 rounded-full bg-success">
+	<div class="p-1 rounded-full bg-success {classes}">
 		<CheckCircleIcon size="14" class="text-white" />
 	</div>
 {/if}
