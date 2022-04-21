8e84cab70f1aeed9bcb2b133642443e3a59d160c
nikolai-cc <nikolai@theministry.co>
2022-04-21 18:02:23 +0200

    filter button

---
+++ src/lib/components/ui/FilterButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { FilterIcon } from 'svelte-feather-icons';
+	import Button from './Button.svelte';
+
+	let classes: string = '';
+	export { classes as class };
+</script>
+
+<Button on:click class={classes} tip="filter">
+	<FilterIcon size="16" />
+</Button>
