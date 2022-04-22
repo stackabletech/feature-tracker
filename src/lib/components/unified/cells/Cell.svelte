3a2be8fdb8fe684df363bd489536aaa4db7bb994
nikolai-cc <nikolai@theministry.co>
2022-04-22 14:37:33 +0200

    disable menu on click outside

--- src/lib/components/unified/cells/Cell.svelte
+++ src/lib/components/unified/cells/Cell.svelte
@@ -20,20 +20,22 @@
 </script>
 
 <div
-	class="flex flex-row items-center transition-all w-full
+	class="flex flex-row items-center transition-all w-full min-w-[12rem]
 		{centered ? 'justify-between' : 'justify-start'}
 		"
 >
 	{#if showMenu}
-		<slot name="indent" />
-		<div class="w-8">
-			<slot name="pre" />
-		</div>
-		<div class="grow">
-			<slot name="menu" />
-		</div>
-		<div class="w-8">
-			<CloseButton on:click={toggleMenu} />
+		<div class="flex flex-row items-between w-full" use:clickOutside on:clickoutside={toggleMenu}>
+			<slot name="indent" />
+			<div class="w-8">
+				<slot name="pre" />
+			</div>
+			<div class="grow">
+				<slot name="menu" />
+			</div>
+			<div class="w-8">
+				<CloseButton on:click={toggleMenu} />
+			</div>
 		</div>
 	{:else if editMode}
 		<div class="mx-2 text-center" use:clickOutside on:clickoutside={toggleEditMode}>
