06cf1557f6c28d180423432973cf957d02f3f4b4
nikolai-cc <nikolai@theministry.co>
2022-04-22 15:55:27 +0200

    add filtering based on children

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -13,8 +13,12 @@ export const categoryFilter: Writable<string> = writable();
 export const productFilter: Writable<string> = writable();
 export const featureFilter: Writable<string> = writable();
 
+export const showUnusedCategories: Writable<boolean> = writable(true);
+export const showUnusedProducts: Writable<boolean> = writable(true);
+export const showUnusedFeatures: Writable<boolean> = writable(true);
+
 // Filtered stores
-export const filter = (store: any, filter: string) => {
+export const stringFilter = (store: any, filter: string) => {
     if (!store) return [];
     if (!filter) return store;
     return store.filter(
@@ -23,16 +27,55 @@ export const filter = (store: any, filter: string) => {
     );
 }
 
-export const filteredCategories = derived([categories, categoryFilter],
-    ([$categories, $categoryFilter]) => filter($categories, $categoryFilter)
+// This store filters out categories using the category fiter string. The remaining categories are filtered out of they have no associated children or features when showUnusedCategories is false.
+export const filteredCategories = derived([categories, categoryFilter, features, showUnusedCategories],
+    ([$categories, $categoryFilter, $features, $showUnusedCategories]) => {
+
+        // Stage 1: Filter out categories based on string filter
+        const categories = stringFilter($categories, $categoryFilter)
+        
+        if ($showUnusedCategories) { return categories }
+
+        // Stage 2: Filter out categories with no children or features
+        return categories.filter((category: Category) => (
+                categories.some((child: Category) => child.parent_id === category.id) ||
+                $features.some(feature => feature.category_id === category.id)
+            )
+        )
+
+    }
 );
 
-export const filteredProducts = derived([products, productFilter],
-    ([$products, $productFilter]) => filter($products, $productFilter)
+export const filteredProducts = derived([products, productFilter, productFeatures, showUnusedProducts],
+    ([$products, $productFilter, $productFeatures, $showUnusedProducts]) => {
+
+        // Stage 1: Filter out products based on string filter
+        const products = stringFilter($products, $productFilter)
+
+        if ($showUnusedProducts) { return products } 
+
+        // Stage 2: Filter out products with no productFeatures
+        return products.filter((product: Product) => (
+                $productFeatures.some(productFeature => productFeature.product_id === product.id)
+            )
+        )
+    }
 );
 
-export const filteredFeatures = derived([features, featureFilter],
-    ([$features, $featureFilter]) => filter($features, $featureFilter)
+export const filteredFeatures = derived([features, featureFilter, productFeatures, showUnusedFeatures],
+    ([$features, $featureFilter, $productFeatures, $showUnusedFeatures]) => {
+        
+        // Stage 1: Filter out features based on string filter
+        stringFilter($features, $featureFilter)
+
+        if ($showUnusedFeatures) { return $features }
+
+        // Stage 2: Filter out features with no productFeatures
+        return $features.filter((feature: Feature) => (
+                $productFeatures.some(productFeature => productFeature.feature_id === feature.id)
+            )
+        )
+    }
 );
 
 // HierarchicalCategory extends the category type with an array of children.
