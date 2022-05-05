8fc387d3c23567e2157de41d54360cc786aca2bc
nikolai-cc <nikolai@theministry.co>
2022-05-05 10:39:12 +0200

    display modal + button

---
+++ src/lib/components/ui/DisplayButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Button from './Button.svelte';
+	import { EyeIcon } from 'svelte-feather-icons';
+</script>
+
+<Button on:click type="btn-outline">
+	<EyeIcon size="16" />
+</Button>
---
+++ src/lib/components/ui/DisplayModal.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Modal from './Modal.svelte';
+
+	import { showUnusedCategories, showUnusedFeatures, showUnusedProducts } from '$lib/stores';
+</script>
+
+<Modal on:close>
+	<h2 class="card-title" slot="title">Display Settings:</h2>
+	<label class="label cursor-pointer">
+		<input type="checkbox" bind:checked={$showUnusedCategories} class="toggle" />
+		<span class="label-text"> Show empty categories </span>
+	</label>
+
+	<label class="label cursor-pointer">
+		<input type="checkbox" bind:checked={$showUnusedFeatures} class="toggle" />
+		<span class="label-text"> Show empty features </span>
+	</label>
+
+	<label class="label cursor-pointer">
+		<input type="checkbox" bind:checked={$showUnusedProducts} class="toggle" />
+		<span class="label-text"> Show empty products </span>
+	</label>
+</Modal>
