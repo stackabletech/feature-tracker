56d665aef40394ae98495cc53c81db4595de3944
nikolai-cc <nikolai@theministry.co>
2022-07-12 10:29:49 +0200

    add portal action

---
+++ src/lib/actions/portal.ts
@@ -0,0 +1,15 @@
+/**
+ * Mounts a component elsewhere in the DOM.
+ * Pass in the target parent by reference or selector.
+ * Usage: <element use:portal={ target: 'main' } />
+ */
+ export const portal = (node: HTMLElement, target: HTMLElement | string) => {
+    typeof target === 'string' && document.querySelector(target)?.appendChild(node)
+
+    return {
+        update: (target: HTMLElement | string) => {
+            typeof target === 'string' ? document.querySelector(target)?.appendChild(node) : target.appendChild(node)
+        },
+        destroy: () => node.parentElement?.removeChild(node)
+    }
+}
