7ad0999adb67696b4e67e33d3091e1fe94f00ea3
nikolai-cc <nikolai@theministry.co>
2022-07-15 12:12:52 +0200

    fix modal heading sizes

--- src/lib/components/ui/HoverNote.svelte
+++ src/lib/components/ui/HoverNote.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { marked } from 'marked';
+	import { fade } from 'svelte/transition';
 	export let note: string;
 </script>
 
-<div class="bg-base-200 base-content p-4 prose rounded-md whitespace-normal shadow-md">
+<div
+	class="bg-base-200 base-content p-4 prose prose-h1:text-lg prose-h2:text-sm rounded-md whitespace-normal shadow-md"
+	transition:fade={{ duration: 200 }}
+>
 	{@html marked.parse(note)}
 </div>
