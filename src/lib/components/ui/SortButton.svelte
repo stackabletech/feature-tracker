77e1dc52d4e84b8d8387a90158e54add0662a549
nikolai-cc <nikolai@theministry.co>
2022-08-02 13:06:47 +0200

    create sort button

---
+++ src/lib/components/ui/SortButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { ListIcon } from 'svelte-feather-icons';
+	import Button from './Button.svelte';
+
+	let classes: string = '';
+	export { classes as class };
+</script>
+
+<Button on:click class={classes} tip="sort">
+	<ListIcon size="16" />
+</Button>
