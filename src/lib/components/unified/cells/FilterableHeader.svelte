e7844fbfc028acb7caa3521e8236297a067334d2
nikolai-cc <nikolai@theministry.co>
2022-08-02 13:08:45 +0200

    add sorting modal to filterable header

--- src/lib/components/unified/cells/FilterableHeader.svelte
+++ src/lib/components/unified/cells/FilterableHeader.svelte
@@ -7,9 +7,15 @@
 	import Header from '$lib/components/unified/cells/Header.svelte';
 	import CloseButton from '$lib/components/ui/CloseButton.svelte';
 	import FilterButton from '$lib/components/ui/FilterButton.svelte';
+	import SortButton from '$lib/components/ui/SortButton.svelte';
+	import Modal from '$lib/components/ui/Modal.svelte';
+	import type { Sortable } from '$lib/stores';
 
 	let input: HTMLInputElement;
 	export let filter: string = '';
+	export let sortable: { [key: string]: Sortable<any> };
+	let hover = false;
+	let modal = false;
 
 	let edit = async () => {
 		editMode = true;
@@ -28,16 +34,33 @@
 		}
 	};
 
+	const showMenu = () => (hover = true);
+	const hideMenu = () => (hover = false);
+	const showModal = () => (modal = true);
+	const hideModal = () => (modal = false);
+
 	let editMode: boolean = filter !== '';
 </script>
 
-<Header sticky class="{classes} left-48 bg-base-300 group" bind:editMode>
+<Header
+	sticky
+	class="{classes} left-48 bg-base-300"
+	bind:editMode
+	on:mouseenter={showMenu}
+	on:mouseleave={hideMenu}
+>
 	<slot name="pre" slot="pre" />
-	<span on:click={edit}>
-		<slot />
-	</span>
 
-	<FilterButton slot="post" on:click={edit} class="opacity-25 group-hover:opacity-100" />
+	{#if hover}
+		<div class="w-full text-center" on:click={hideMenu}>
+			<SortButton on:click={showModal} />
+			<FilterButton on:click={edit} />
+		</div>
+	{:else}
+		<span on:click={edit}>
+			<slot />
+		</span>
+	{/if}
 
 	<div slot="edit" class="input-group input-group-sm">
 		<input
@@ -52,3 +75,34 @@
 	</div>
 	<slot name="note" slot="note" />
 </Header>
+
+{#if modal}
+	<Modal on:close={hideModal}>
+		<h3 class="font-bold" slot="title">Sort</h3>
+		{#each Object.entries(sortable) as [name, store]}
+			<span class="font-bold">{name}:</span>
+			<div class="btn-group">
+				<button
+					class="btn btn-xs btn-outline"
+					on:click={store.sort('name', 'asc')}
+					on:click={hideModal}>name: a-z</button
+				>
+				<button
+					class="btn btn-xs btn-outline"
+					on:click={store.sort('name', 'desc')}
+					on:click={hideModal}>name: z-a</button
+				>
+				<button
+					class="btn btn-xs btn-outline"
+					on:click={store.sort('id', 'asc')}
+					on:click={hideModal}>id: 123</button
+				>
+				<button
+					class="btn btn-xs btn-outline"
+					on:click={store.sort('id', 'desc')}
+					on:click={hideModal}>id: 321</button
+				>
+			</div>
+		{/each}
+	</Modal>
+{/if}
