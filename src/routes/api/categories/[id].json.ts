6982b1cb8dfa5c68fdcd7fde4a8c524ce6adc84f
nikolai-cc <nikolai@theministry.co>
2022-08-09 14:40:36 +0200

    disallow setting category as it's own parent

--- src/routes/api/categories/[id].json.ts
+++ src/routes/api/categories/[id].json.ts
@@ -22,6 +22,16 @@ export const PATCH = async ({ params, request }) => {
     const data = await request.json()
     data.parent_id = data.parent_id || null
     const id = parseInt(params.id);
+
+    if (id === data.parent_id) {
+        return {
+            status: 400,
+            body: {
+                code: "INVALID_PARENT_ID",
+                message: "Cannot set parent_id to the same value as the category id"
+            }
+        }
+    }
     
     try {
         const body = await prisma.category.update({
