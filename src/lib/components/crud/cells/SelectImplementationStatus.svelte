e71125948c47df8eb3039cfa3933ff87b4e4b6b4
nikolai-cc <nikolai@theministry.co>
2022-08-09 14:24:55 +0200

    implement implementationstatus selection

--- src/lib/components/crud/cells/SelectImplementationStatus.svelte
+++ src/lib/components/crud/cells/SelectImplementationStatus.svelte
@@ -1,11 +1,4 @@
 <script lang="ts">
-	import Select from './Select.svelte';
-	import type { ImplementationStatus } from '$lib/prisma';
-
-	export let value: ImplementationStatus;
-	export let colspan: number = 1;
-	export let rowspan: number = 1;
-
 	const options = [
 		{ value: 'NOT_PLANNED', label: 'Not Planned' },
 		{ value: 'PLANNED', label: 'Planned' },
@@ -13,6 +6,54 @@
 		{ value: 'COMPLETED', label: 'Completed' },
 		{ value: 'NOT_AVAILABLE', label: 'Not Available' }
 	];
+
+	import Option from '$lib/components/crud/cells/Option.svelte';
+	import type { ImplementationStatus } from '@prisma/client';
+	import { ChevronDownIcon } from 'svelte-feather-icons';
+
+	export let value: ImplementationStatus = undefined;
+	export let colspan: number;
+	export let rowspan: number;
+	export let optional = false;
+
+	let title = options.find((c) => c.value === value)?.label ?? 'Select Implementation Status';
+	let menu: HTMLElement;
+
+	let select = (e: CustomEvent) => {
+		title = e.detail.label;
+		value = e.detail.value;
+		console.log('selected', value);
+		menu.blur();
+	};
 </script>
 
-<Select bind:value {options} {colspan} {rowspan} />
+<td {colspan} {rowspan}>
+	<div class="dropdown">
+		<div
+			tabindex="0"
+			class="btn btn-ghost border border-base-300 bg-base-100 flex flex-row justify-between m-1 w-64 group"
+		>
+			<span>
+				{title}
+			</span>
+			<ChevronDownIcon class="group-focus-within:rotate-180 transition-all" size="16" />
+		</div>
+		<ul
+			tabindex="0"
+			class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-96"
+			bind:this={menu}
+		>
+			{#if optional}
+				<Option
+					value={undefined}
+					prefix=""
+					label="No Implementation Status"
+					on:select={(e) => select(e)}
+				/>
+			{/if}
+			{#each options as { value, label }}
+				<Option {value} {label} on:select={(e) => select(e)} />
+			{/each}
+		</ul>
+	</div>
+</td>
