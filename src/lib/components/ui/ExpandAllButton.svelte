85ab1dc75490061d91436287b84ef066e1d636e3
nikolai-cc <nikolai@theministry.co>
2022-08-16 16:48:51 +0200

    create expandAll button component

---
+++ src/lib/components/ui/ExpandAllButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { ChevronsRightIcon } from 'svelte-feather-icons';
+	import Button from './Button.svelte';
+
+	export let collapse = false;
+	$: tip = collapse ? 'collapse all' : 'expand all';
+</script>
+
+<Button on:click {tip} type="btn-outline">
+	<ChevronsRightIcon size="16" class="transition-all {collapse || 'rotate-90'}" />
+</Button>
