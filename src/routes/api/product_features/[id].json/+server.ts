a03d3d88178207fcf28bed0a6ba836901e69b02e
nikolai-cc <nikolai@theministry.co>
2022-08-16 13:22:07 +0200

    [svelte-migrate] resolve comment

--- src/routes/api/product_features/[id].json/+server.ts
+++ src/routes/api/product_features/[id].json/+server.ts
@@ -1,3 +1,4 @@
+import { json } from '@sveltejs/kit';
 import { prisma } from "$lib/prisma";
 
 // GET /product_features/:id.json
@@ -8,12 +9,11 @@ export const GET = async ({ params }) => {
         const body = await prisma.productFeature.findUnique({
             where: { id }
         })
-        return { body }
+        return json(body);
     } catch ({ code, message }) {
-        return {
-            status: 500,
-            body: { code, message }
-        }
+        return json({ code, message }, {
+            status: 500
+        })
     }
 }
 
@@ -27,12 +27,11 @@ export const PATCH = async ({ params, request }) => {
             where: { id },
             data
         })
-        return { body }
+        return json(body);
     } catch ({ code, message }) {
-        return {
-            status: 500,
-            body: { code, message }
-        }
+        return json({ code, message }, {
+            status: 500
+        })
     }
 }
 
@@ -44,11 +43,10 @@ export const DELETE = async ({ params }) => {
         const body = await prisma.productFeature.delete({
             where: { id }
         })
-        return { body }
+        return json(body);
     } catch ({ code, message }) {
-        return {
-            status: 500,
-            body: { code, message }
-        }
+        return json({ code, message }, {
+            status: 500
+        })
     }
 }
