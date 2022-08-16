8a4935f23f0553552f72803399c3ee31c7ffca96
nikolai-cc <nikolai@theministry.co>
2022-08-16 13:32:55 +0200

    [svelte-migrate] resolve comment

--- src/routes/edit/features/+page.svelte
+++ src/routes/edit/features/+page.svelte
@@ -1,16 +1,6 @@
-<!-- <script context="module" lang="ts">
-	export const load = async ({ fetch }) => {
-		const response = await fetch('/api/features.json');
-		const features = await response.json();
-		return { props: { features } };
-	};
-</script> -->
 <script lang="ts">
 	import { features } from '$lib/stores';
-
 	import Table from '$lib/components/crud/Table.svelte';
-	import type { Feature } from '$lib/prisma';
-	// export let features: Feature[];
 </script>
 
 <Table bind:data={$features} endpoint="/api/features" />
