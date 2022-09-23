f47a0e9b03b668a7c34cdfff00425b5975a5e94d
nikolai-cc <nikolai@theministry.co>
2022-09-23 15:24:18 +0200

    add release API endpoint

---
+++ src/routes/api/releases.json/+server.ts
@@ -0,0 +1,34 @@
+import { json } from '@sveltejs/kit';
+import { prisma } from "$lib/prisma";
+// import type { Release } from "@prisma/client";
+
+/*
+    Typing disabled because of this bug: https://github.com/prisma/prisma/issues/10404
+*/
+
+// GET /releases.json
+export const GET = async ({params, request, url}) => {
+    try {
+        const body = await prisma.release.findMany({
+            orderBy: { date: 'desc' }
+        });
+        return json(body);
+    } catch ({ code, message }) {
+        return json({ code, message }, {
+            status: 500
+        })
+    }
+};
+
+// POST /releases.json
+export const POST = async ({ request }) => {
+    const data = await request.json()
+    try {
+        const body = await prisma.release.create({ data });
+        return json(body);
+    } catch ({ code, message }) {
+        return json({ code, message }, {
+            status: 500
+        })
+    }
+}
