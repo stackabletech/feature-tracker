aeda1ee141830bf98df11b4a4fef038d1fa6dfbb
nikolai-cc <nikolai@theministry.co>
2022-09-23 18:46:41 +0200

    improve readability

--- src/lib/components/ui/ImplementationIcon.svelte
+++ src/lib/components/ui/ImplementationIcon.svelte
@@ -11,29 +11,29 @@
 
 {#if status == 'NOT_PLANNED'}
 	<div class="p-1 rounded-full bg-error {classes}">
-		<XCircleIcon size="14" class="text-white" />
+		<XCircleIcon size="14" class="text-error-content" />
 	</div>
 {:else if status == 'PLANNED'}
 	<div class="p-1 rounded-full bg-info {classes}">
-		<CalendarIcon size="14" class="text-white" />
+		<CalendarIcon size="14" class="text-info-content" />
 	</div>
 {:else if status == 'IN_PROGRESS'}
 	<div class="p-1 rounded-full bg-warning {classes}">
-		<ActivityIcon size="14" class="text-white" />
+		<ActivityIcon size="14" class="text-warning-content" />
 	</div>
 {:else if status == 'COMPLETED'}
 	{#if released}
 		<div class="p-1 rounded-full bg-success {classes}">
-			<CheckCircleIcon size="14" class="text-white" />
+			<CheckCircleIcon size="14" class="text-success-content" />
 		</div>
 	{:else}
-		<div class="p-1 rounded-full bg-white border border-success {classes}">
+		<div class="p-1 rounded-full bg-base border border-success {classes}">
 			<CheckCircleIcon size="14" class="text-success" />
 		</div>
 	{/if}
 {:else if status == 'NOT_AVAILABLE'}
 	<div
-		class="rounded-full bg-base-300 text-white w-[22px] h-[22px] text-[9px] leading-[22px] font-bold flex align-middle justify-center {classes}"
+		class="rounded-full bg-base-300 text-base w-[22px] h-[22px] text-[8px] leading-[22px] font-bold flex align-middle justify-center {classes}"
 	>
 		N/A
 	</div>
