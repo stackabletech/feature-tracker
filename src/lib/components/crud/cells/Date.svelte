6d4a9b067d66339fdb59d8cb3d58a6ee7c111d96
nikolai-cc <nikolai@theministry.co>
2022-10-06 14:48:47 +0200

    new Date selector (using native input type=date)

--- src/lib/components/crud/cells/Date.svelte
+++ src/lib/components/crud/cells/Date.svelte
@@ -7,24 +7,9 @@
 	export let rowspan: number = 1;
 	export let colspan: number = 1;
 
-	let date = new Date(value || Date.now());
-	$: month = date.getMonth() + 1;
-	$: year = date.getFullYear();
+	let date = new Date(value || Date.now()).toISOString().split('T')[0];
 
-	$: inputValue = `${year}-${month}`;
-	$: value = checked ? date.toISOString() : null;
-
-	let updateValue = (d: number) => {
-		date = new Date(date.setMonth(date.getMonth() + d));
-	};
-
-	let handleInput = (e: KeyboardEvent) => {
-		if (e.key == 'Escape' || e.key == 'Tab') return;
-
-		e.preventDefault();
-		(e.key == 'ArrowUp' || e.key == 'ArrowRight') && updateValue(1);
-		(e.key == 'ArrowDown' || e.key == 'ArrowLeft') && updateValue(-1);
-	};
+	$: value = checked ? new Date(date).toISOString() : null;
 </script>
 
 <td {colspan} {rowspan}>
@@ -37,24 +22,11 @@
 				<input type="checkbox" bind:checked class="checkbox" />
 			</div>
 		{/if}
-		<div class="input-group">
-			<button
-				class="btn btn-ghost focus:btn-outline"
-				on:click={() => updateValue(-1)}
-				disabled={!checked}>-</button
-			>
-			<input
-				type="text"
-				class="input disabled:text-white disabled:text-opacity-20"
-				bind:value={inputValue}
-				on:keydown={handleInput}
-				disabled={!checked}
-			/>
-			<button
-				class="btn btn-ghost focus:btn-outline"
-				on:click={() => updateValue(1)}
-				disabled={!checked}>+</button
-			>
-		</div>
+		<input
+			type="date"
+			class="input disabled:text-white disabled:text-opacity-20"
+			bind:value={date}
+			disabled={!checked}
+		/>
 	</div>
 </td>
