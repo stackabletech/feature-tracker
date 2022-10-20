66be5b9d3a0092cd01c8b87b97511993c815627a
nikolai-cc <nikolai@theministry.co>
2022-10-20 15:21:16 +0200

    add released state to RoadMapCell

--- src/lib/components/unified/cells/RoadMapCell.svelte
+++ src/lib/components/unified/cells/RoadMapCell.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { features, products } from '$lib/stores';
 	import ProductFeatureModal from '$lib/components/ui/ProductFeatureModal.svelte';
-	import type { ProductFeature, Feature, Product } from '@prisma/client';
+	import type { ProductFeature, Feature, Product, Release } from '@prisma/client';
 
 	export let productFeature: ProductFeature;
+	export let release: Release;
+
 	let feature: Feature = $features.find((feature) => feature.id === productFeature.feature_id);
 	let product: Product = $products.find((product) => product.id === productFeature.product_id);
 
@@ -19,7 +21,11 @@
 </script>
 
 <div class="tooltip cursor-pointer" data-tip={feature.name} on:click={showInfo}>
-	<ImplementationIcon status={productFeature.implementation_status} class="mx-1 my-auto" />
+	<ImplementationIcon
+		status={productFeature.implementation_status}
+		released={release.released}
+		class="mx-1 my-auto"
+	/>
 </div>
 
 {#if modal}
