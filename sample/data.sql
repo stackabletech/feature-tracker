add6058975cee9f5b6aba489458762e4fe236025
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-01-05 10:44:15 +0100

    Added container_images relation

--- sample/data.sql
+++ sample/data.sql
@@ -3,43 +3,47 @@ insert into products (name) values ('airflow'), ('druid'), ('trino');
 --
 -- Insert data for release 23.11.0
 --
-insert into releases (name, date, released) values ('23.11.0', '2023-11-29', true);
+insert into releases (id, name, date, released) values (1, '23.11.0', '2023-11-29', true);
 
-insert into product_lifecycles (product_id, release_id, product_version, image_url, support_status)
+insert into container_images (purl, product_version, product_id) values
+('pkg:docker/stackable/airflow:2.6.1-stackable23.11.0?repository_url=docker.stackable.tech', '2.6.1', 1),
+('pkg:docker/stackable/airflow:2.6.3-stackable23.11.0?repository_url=docker.stackable.tech', '2.6.3', 1),
+('pkg:docker/stackable/airflow:2.7.2-stackable23.11.0?repository_url=docker.stackable.tech', '2.7.2', 1),
+('pkg:docker/stackable/druid:26.0.0-stackable23.11.0?repository_url=docker.stackable.tech', '26.0.0', 2),
+('pkg:docker/stackable/druid:27.0.0-stackable23.11.0?repository_url=docker.stackable.tech', '27.0.0', 2),
+('pkg:docker/stackable/trino:414-stackable23.11.0?repository_url=docker.stackable.tech', '414', 3),
+('pkg:docker/stackable/trino:428-stackable23.11.0?repository_url=docker.stackable.tech', '428', 3);
+
+insert into product_lifecycles (release_id, container_image_id, support_status)
 values
-(1, 1, '2.6.1', 'docker.stackable.tech/stackable/airflow:2.6.1-stackable23.11.0', 'DEPRECATED'),
-(1, 1, '2.6.3', 'docker.stackable.tech/stackable/airflow:2.6.3-stackable23.11.0', 'LONG_TERM_SUPPORT'),
-(1, 1, '2.7.2', 'docker.stackable.tech/stackable/airflow:2.7.2-stackable23.11.0', 'GENERAL_AVAILABILITY');
-
-insert into product_lifecycles (product_id, release_id, product_version, image_url, support_status)
-values
-(2, 1, '26.0.0', 'docker.stackable.tech/stackable/druid:26.0.0-stackable23.11.0', 'LONG_TERM_SUPPORT'),
-(2, 1, '27.0.0', 'docker.stackable.tech/stackable/druid:27.0.0-stackable23.11.0', 'GENERAL_AVAILABILITY');
-
-
-insert into product_lifecycles (product_id, release_id, product_version, image_url, support_status)
-values
-(3, 1, '414', 'docker.stackable.tech/stackable/trino:414-stackable23.11.0', 'DEPRECATED'),
-(3, 1, '428', 'docker.stackable.tech/stackable/trino:428-stackable23.11.0', 'GENERAL_AVAILABILITY');
+(1, 'pkg:docker/stackable/airflow:2.6.1-stackable23.11.0?repository_url=docker.stackable.tech', 'DEPRECATED'),
+(1, 'pkg:docker/stackable/airflow:2.6.3-stackable23.11.0?repository_url=docker.stackable.tech', 'LONG_TERM_SUPPORT'),
+(1, 'pkg:docker/stackable/airflow:2.7.2-stackable23.11.0?repository_url=docker.stackable.tech', 'GENERAL_AVAILABILITY'),
+(1, 'pkg:docker/stackable/druid:26.0.0-stackable23.11.0?repository_url=docker.stackable.tech', 'GENERAL_AVAILABILITY'),
+(1, 'pkg:docker/stackable/druid:27.0.0-stackable23.11.0?repository_url=docker.stackable.tech', 'GENERAL_AVAILABILITY'),
+(1, 'pkg:docker/stackable/trino:414-stackable23.11.0?repository_url=docker.stackable.tech', 'DEPRECATED'),
+(1, 'pkg:docker/stackable/trino:428-stackable23.11.0?repository_url=docker.stackable.tech', 'GENERAL_AVAILABILITY');
 
 --
 -- Insert data for release 23.7.0
 --
-insert into releases (name, date, released) values ('23.7.0', '2023-07-28', true);
+insert into releases (id, name, date, released) values (2, '23.7.0', '2023-07-28', true);
 
-insert into product_lifecycles (product_id, release_id, product_version, image_url, support_status)
+insert into container_images (purl, product_version, product_id) values
+('pkg:docker/stackable/airflow:2.4.1-stackable23.7.0?repository_url=docker.stackable.tech', '2.4.1', 1),
+('pkg:docker/stackable/airflow:2.6.1-stackable23.7.0?repository_url=docker.stackable.tech', '2.6.1', 1),
+('pkg:docker/stackable/druid:24.0.0-stackable23.7.0?repository_url=docker.stackable.tech', '24.0.0', 2),
+('pkg:docker/stackable/druid:26.0.0-stackable23.7.0?repository_url=docker.stackable.tech', '26.0.0', 2),
+('pkg:docker/stackable/trino:414-stackable23.7.0?repository_url=docker.stackable.tech', '414', 3),
+('pkg:docker/stackable/trino:403-stackable23.7.0?repository_url=docker.stackable.tech', '403', 3);
+
+insert into product_lifecycles (release_id, container_image_id, support_status)
 values
-(1, 2, '2.4.1', 'docker.stackable.tech/stackable/airflow:2.4.1-stackable23.7.0', 'DEPRECATED'),
-(1, 2, '2.6.1', 'docker.stackable.tech/stackable/airflow:2.6.1-stackable23.7.0', 'GENERAL_AVAILABILITY');
+(2, 'pkg:docker/stackable/airflow:2.6.1-stackable23.7.0?repository_url=docker.stackable.tech', 'DEPRECATED'),
+(2, 'pkg:docker/stackable/airflow:2.4.1-stackable23.7.0?repository_url=docker.stackable.tech', 'LONG_TERM_SUPPORT'),
+(2, 'pkg:docker/stackable/druid:26.0.0-stackable23.7.0?repository_url=docker.stackable.tech', 'GENERAL_AVAILABILITY'),
+(2, 'pkg:docker/stackable/druid:24.0.0-stackable23.7.0?repository_url=docker.stackable.tech', 'GENERAL_AVAILABILITY'),
+(2, 'pkg:docker/stackable/trino:414-stackable23.7.0?repository_url=docker.stackable.tech', 'DEPRECATED'),
+(2, 'pkg:docker/stackable/trino:403-stackable23.7.0?repository_url=docker.stackable.tech', 'GENERAL_AVAILABILITY');
 
-insert into product_lifecycles (product_id, release_id, product_version, image_url, support_status)
-values
-(2, 2, '24.0.0', 'docker.stackable.tech/stackable/druid:24.0.0-stackable23.7.0', 'DEPRECATED'),
-(2, 2, '26.0.0', 'docker.stackable.tech/stackable/druid:26.0.0-stackable23.7.0', 'LONG_TERM_SUPPORT');
-
-
-insert into product_lifecycles (product_id, release_id, product_version, image_url, support_status)
-values
-(3, 2, '414', 'docker.stackable.tech/stackable/trino:414-stackable23.7.0', 'GENERAL_AVAILABILITY'),
-(3, 2, '403', 'docker.stackable.tech/stackable/trino:403-stackable23.7.0', 'DEPRECATED');
 
--- sample/queries.sql
+++ sample/queries.sql
@@ -1,15 +1,18 @@
 
 -- Get all product names and images for release 23.11.0
-select rel.name, p.name, pl.image_url
-from product_lifecycles pl
+select rel.name, p.name, pl.support_status, ci.purl
+from container_images ci
+join product_lifecycles pl on ci.purl = pl.container_image_id
 join releases rel on pl.release_id = rel.id
-join products p on pl.product_id=p.id
+join products p on ci.product_id = p.id
 where rel.name = '23.11.0';
 
 -- Get all trino images for release 23.11.0
-select rel.name, p.name, pl.image_url
-from product_lifecycles pl
+select rel.name, p.name, pl.support_status, ci.purl
+from container_images ci
+join product_lifecycles pl on ci.purl = pl.container_image_id
 join releases rel on pl.release_id = rel.id
-join products p on pl.product_id=p.id
+join products p on ci.product_id = p.id
 where rel.name = '23.11.0' and p.name = 'trino';
+
 
