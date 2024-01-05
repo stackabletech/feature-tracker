4952c54ce7a2dbfd57cb9513b5b391371578ea8b
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2024-01-05 17:05:54 +0100

    Update data and query with the DB baseline

--- sample/data.sql
+++ sample/data.sql
@@ -1,31 +1,33 @@
-insert into products (id, name) values (1, 'airflow'), (2, 'druid'), (3, 'trino');
+
+-- set the default schema
+set search_path to feature_tracker;
 
 insert into product_versions (id, product_id, version) values
-(1, 1, '2.6.1'),
-(2, 1, '2.6.3'),
-(3, 1, '2.7.2'),
-(4, 2, '26.0.0'),
-(5, 2, '27.0.0'),
-(6, 3, '414'),
-(7, 3, '428'),
-(8, 1, '2.4.1'),
-(9, 2, '24.0.0'),
-(10, 3, '403');
+(1, 38, '2.6.1'),
+(2, 38, '2.6.3'),
+(3, 38, '2.7.2'),
+(4, 39, '26.0.0'),
+(5, 39, '27.0.0'),
+(6, 45, '414'),
+(7, 45, '428'),
+(8, 38, '2.4.1'),
+(9, 39, '24.0.0'),
+(10, 45, '403');
 
 
 --
 -- Insert data for release 23.11 (id=7)
 --
-insert into release_versions (version, release_id, created_at)  values ('23.11.0', 1, '2023-11-29');
+insert into release_versions (version, release_id, created_at)  values ('23.11.0', 7, '2023-11-29');
 
-insert into release_component (purl, product_version_id, release_version) values
+insert into release_components (purl, product_version_id, release_version) values
 ('pkg:docker/stackable/airflow:2.6.1-stackable23.11.0?repository_url=docker.stackable.tech', 1, '23.11.0'),
 ('pkg:docker/stackable/airflow:2.6.3-stackable23.11.0?repository_url=docker.stackable.tech', 2, '23.11.0'),
 ('pkg:docker/stackable/airflow:2.7.2-stackable23.11.0?repository_url=docker.stackable.tech', 3, '23.11.0'),
 ('pkg:docker/stackable/druid:26.0.0-stackable23.11.0?repository_url=docker.stackable.tech', 4, '23.11.0'),
 ('pkg:docker/stackable/druid:27.0.0-stackable23.11.0?repository_url=docker.stackable.tech', 5, '23.11.0'),
 ('pkg:docker/stackable/trino:414-stackable23.11.0?repository_url=docker.stackable.tech', 6, '23.11.0'),
-('pkg:docker/stackable/trino:428-stackable23.11.0?repository_url=docker.stackable.tech', 7, '23.11.0'),
+('pkg:docker/stackable/trino:428-stackable23.11.0?repository_url=docker.stackable.tech', 7, '23.11.0');
 
 insert into product_lifecycles (support_status, product_version_id, release_id)
 values
@@ -41,9 +43,9 @@ values
 -- Insert data for release 23.7 (id=6)
 --
 
-insert into release_versions (version, release_id, created_at)  values ('23.7.0', 1, '2023-07-29');
+insert into release_versions (version, release_id, created_at)  values ('23.7.0', 6, '2023-07-25');
 
-insert into release_component (purl, product_version_id, release_version) values
+insert into release_components (purl, product_version_id, release_version) values
 ('pkg:docker/stackable/airflow:2.4.1-stackable23.7.0?repository_url=docker.stackable.tech', 8, '23.7.0'),
 ('pkg:docker/stackable/airflow:2.6.1-stackable23.7.0?repository_url=docker.stackable.tech', 1, '23.7.0'),
 ('pkg:docker/stackable/druid:24.0.0-stackable23.7.0?repository_url=docker.stackable.tech', 9,  '23.7.0'),
--- sample/queries.sql
+++ sample/queries.sql
@@ -1,18 +1,48 @@
+set search_path to feature_tracker;
 
--- Get all product names and images for release 23.11.0
-select rel.name, p.name, pl.support_status, ci.purl
-from container_images ci
-join product_lifecycles pl on ci.purl = pl.container_image_id
-join releases rel on pl.release_id = rel.id
-join products p on ci.product_id = p.id
-where rel.name = '23.11.0';
+-- Get all release versions of release 2023-11
+select rel.name, rev.version, rev.created_at
+from releases rel
+join release_versions rev on rel.id = rev.release_id
+where rel.name = '2023-11';
 
--- Get all trino images for release 23.11.0
-select rel.name, p.name, pl.support_status, ci.purl
-from container_images ci
-join product_lifecycles pl on ci.purl = pl.container_image_id
-join releases rel on pl.release_id = rel.id
-join products p on ci.product_id = p.id
-where rel.name = '23.11.0' and p.name = 'trino';
+--  name   | version |     created_at      
+-- --------+---------+---------------------
+-- 2023-11 | 23.11.0 | 2023-11-29 00:00:00
 
+-- Get the support status of all product versions in release 2023-11
+select rel.name as release_name, p.name as product_name, pv.version, pl.support_status
+from releases rel
+join product_lifecycles pl on pl.release_id = rel.id
+join product_versions pv on pl.product_version_id = pv.id
+join products p on pv.product_id = p.id
+where rel.name = '2023-11';
+
+-- release_name |  product_name  | version |    support_status    
+-- -------------+----------------+---------+----------------------
+-- 2023-11      | Apache Airflow | 2.6.1   | DEPRECATED
+-- 2023-11      | Apache Airflow | 2.6.3   | LONG_TERM_SUPPORT
+-- 2023-11      | Apache Airflow | 2.7.2   | GENERAL_AVAILABILITY
+-- 2023-11      | Apache Druid   | 26.0.0  | GENERAL_AVAILABILITY
+-- 2023-11      | Apache Druid   | 27.0.0  | GENERAL_AVAILABILITY
+-- 2023-11      | Trino          | 414     | DEPRECATED
+-- 2023-11      | Trino          | 428     | GENERAL_AVAILABILITY
+
+-- Get all release components (images) for all versions of release version 23.11.0
+select rev.version as release_version, p.name as product_name, pv.version, rc.purl
+from release_versions rev
+join release_components rc on rc.release_version = rev.version
+join product_versions pv on pv.id = rc.product_version_id
+join products p on pv.product_id = p.id
+where rev.version = '23.11.0';
+
+--  release_version |  product_name  | version |           purl                                           
+-- -----------------+----------------+---------+--------------------------------
+--  23.11.0         | Apache Airflow | 2.6.1   | pkg:docker/stackable/airflow...
+--  23.11.0         | Apache Airflow | 2.6.3   | pkg:docker/stackable/airflow... 
+--  23.11.0         | Apache Airflow | 2.7.2   | pkg:docker/stackable/airflow... 
+--  23.11.0         | Apache Druid   | 26.0.0  | pkg:docker/stackable/druid:2... 
+--  23.11.0         | Apache Druid   | 27.0.0  | pkg:docker/stackable/druid:2... 
+--  23.11.0         | Trino          | 414     | pkg:docker/stackable/trino:4... 
+--  23.11.0         | Trino          | 428     | pkg:docker/stackable/trino:4... 
 
