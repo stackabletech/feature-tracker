0dbca8bdd2bf9748106bf2d9d9cdb91fefa041e6
Lars Francke <git@lars-francke.de>
2024-05-28 15:48:44 +0200

    Fix a few errors

--- src/lib/components/unified/cells/Cell.svelte
+++ src/lib/components/unified/cells/Cell.svelte
@@ -1,79 +1,79 @@
 <script lang="ts">
-	import { portal } from '$lib/actions/portal';
-	import { clickOutside } from '$lib/actions/clickOutside';
+  import {portal} from '$lib/actions/portal';
+  import {clickOutside} from '$lib/actions/clickOutside';
 
-	import CloseButton from '$lib/components/ui/CloseButton.svelte';
-	import MenuButton from '$lib/components/ui/MenuButton.svelte';
+  import CloseButton from '$lib/components/ui/CloseButton.svelte';
+  import MenuButton from '$lib/components/ui/MenuButton.svelte';
 
-	export let menu: boolean = false;
-	export let showMenu: boolean = false;
+  export let menu: boolean = false;
+  export let showMenu: boolean = false;
 
-	export let editMode: boolean = false;
-	export let centered: boolean = false;
+  export let editMode: boolean = false;
+  export let centered: boolean = false;
 
-	let toggleMenu = () => {
-		showMenu = !showMenu;
-	};
+  let toggleMenu = () => {
+    showMenu = !showMenu;
+  };
 
-	let toggleEditMode = () => {
-		editMode = false;
-	};
+  let toggleEditMode = () => {
+    editMode = false;
+  };
 
-	let showNote = () => {
-		note = true;
-	};
+  let showNote = () => {
+    note = true;
+  };
 
-	let hideNote = () => {
-		note = false;
-	};
+  let hideNote = () => {
+    note = false;
+  };
 
-	let note = false;
+  let note = false;
 </script>
 
 <div
-	class="flex flex-row items-center transition-all w-full min-w-[12rem]
+  class="flex flex-row items-center transition-all w-full min-w-[12rem]
 		{centered ? 'justify-between' : 'justify-start'}
 		"
-	on:pointerover={showNote}
-	on:pointerleave={hideNote}
+  on:pointerover={showNote}
+  on:pointerleave={hideNote}
 >
-	{#if showMenu}
-		<div class="flex flex-row items-between w-full" use:clickOutside on:clickoutside={toggleMenu}>
-			<slot name="indent" />
-			<div class="w-8">
-				<slot name="pre" />
-			</div>
-			<div class="grow">
-				<slot name="menu" />
-			</div>
-			<div class="w-8">
-				<CloseButton on:click={toggleMenu} />
-			</div>
-		</div>
-	{:else if editMode}
-		<div class="mx-2 text-center" use:clickOutside on:clickoutside={toggleEditMode}>
-			<slot name="edit" />
-		</div>
-	{:else}
-		<slot name="indent" />
-		<div class="w-8">
-			<slot name="pre" />
-		</div>
-		<div class="mx-2 grow {centered && 'text-center'}">
-			<slot />
-		</div>
-		<div class="w-8">
-			{#if menu}
-				<MenuButton on:click={toggleMenu} />
-			{:else}
-				<slot name="post" />
-			{/if}
-		</div>
-	{/if}
+  {#if showMenu}
+    <div class="flex flex-row items-between w-full" use:clickOutside on:clickoutside={toggleMenu}>
+      <slot name="indent"/>
+      <div class="w-8">
+        <slot name="pre"/>
+      </div>
+      <div class="grow">
+        <slot name="menu"/>
+      </div>
+      <div class="w-8">
+        <CloseButton on:click={toggleMenu}/>
+      </div>
+    </div>
+  {:else if editMode}
+    <div class="mx-2 text-center" use:clickOutside on:clickoutside={toggleEditMode}>
+      <slot name="edit"/>
+    </div>
+  {:else}
+    <slot name="indent"/>
+    <div class="w-8">
+      <slot name="pre"/>
+    </div>
+    <div class="mx-2 grow {centered && 'text-center'}">
+      <slot/>
+    </div>
+    <div class="w-8">
+      {#if menu}
+        <MenuButton on:click={toggleMenu}/>
+      {:else}
+        <slot name="post"/>
+      {/if}
+    </div>
+  {/if}
 </div>
 
 {#if note}
-	<div class="note" use:portal={'#mouse'}>
-		<slot name="note" />
-	</div>
+  <div class="note" use:portal={'#mouse'}>
+    <slot name="note"/>
+  </div>
 {/if}
--- src/lib/components/unified/cells/TextCell.svelte
+++ src/lib/components/unified/cells/TextCell.svelte
@@ -1,53 +1,53 @@
 <script lang="ts">
-	import TextInput from '$lib/components/ui/TextInput.svelte';
-	import { createEventDispatcher } from 'svelte';
+  import TextInput from '$lib/components/ui/TextInput.svelte';
+  import {createEventDispatcher} from 'svelte';
 
-	import type { Writable } from 'svelte/store';
-	import { getContext } from 'svelte';
+  import type {Writable} from 'svelte/store';
+  import {getContext} from 'svelte';
 
-	let editable: Writable<boolean> = getContext('editable');
+  let editable: Writable<boolean> = getContext('editable');
 
-	let dispatch = createEventDispatcher();
+  let dispatch = createEventDispatcher();
 
-	import Header from '../cells/Header.svelte';
-	import Data from '../cells/Data.svelte';
+  import Header from '../cells/Header.svelte';
+  import Data from '../cells/Data.svelte';
 
-	let classes: string = '';
-	export { classes as class };
+  let classes: string = '';
+  export {classes as class};
 
-	export let value: string = '';
-	export let menu: boolean = false;
-	export let sticky: boolean = false;
-	export let showMenu: boolean = false;
+  export let value: string = '';
+  export let menu: boolean = false;
+  export let sticky: boolean = false;
+  export let showMenu: boolean = false;
 
-	export let header: boolean = true;
-	$: type = header ? Header : Data;
+  export let header: boolean = true;
+  $: type = header ? Header : Data;
 
-	let editMode: boolean = false;
+  let editMode: boolean = false;
 
-	let toggleEditMode = () => {
-		editMode = $editable && !editMode;
-	};
+  let toggleEditMode = () => {
+    editMode = $editable && !editMode;
+  };
 
-	let handleSubmit = (e: CustomEvent) => {
-		value = e.detail.value;
-		dispatch('update', { value });
-		toggleEditMode();
-	};
+  let handleSubmit = (e: CustomEvent) => {
+    value = e.detail.value;
+    dispatch('update', {value});
+    toggleEditMode();
+  };
 
-	let handleCancel = () => {
-		toggleEditMode();
-	};
+  let handleCancel = () => {
+    toggleEditMode();
+  };
 </script>
 
 <svelte:component this={type} {menu} bind:editMode {sticky} bind:showMenu class={classes}>
-	<slot name="indent" slot="indent" />
-	<slot name="pre" slot="pre" />
-	<svelte:fragment slot="edit">
-		<TextInput {value} on:submit={handleSubmit} on:cancel={handleCancel} />
-	</svelte:fragment>
-	<span on:click={toggleEditMode} class={$editable && 'cursor-cell'}>{value}</span>
-	<slot name="menu" slot="menu" />
-	<slot name="note" slot="note" />
-	<slot name="post" slot="post" />
+  <slot name="indent" slot="indent"/>
+  <slot name="pre" slot="pre"/>
+  <svelte:fragment slot="edit">
+    <TextInput {value} on:submit={handleSubmit} on:cancel={handleCancel}/>
+  </svelte:fragment>
+  <span on:click={toggleEditMode} class={$editable && 'cursor-cell'}>{value}</span>
+  <slot name="menu" slot="menu"/>
+  <slot name="note" slot="note"/>
+  <slot name="post" slot="post"/>
 </svelte:component>
