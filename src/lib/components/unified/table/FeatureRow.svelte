535e608fd35a5a90f06179057b2961a12279bacf
Lars Francke <git@lars-francke.de>
2024-05-28 12:09:19 +0200

    Fix a few errors

--- src/lib/components/unified/table/FeatureRow.svelte
+++ src/lib/components/unified/table/FeatureRow.svelte
@@ -1,29 +1,29 @@
 <script lang="ts">
-	import { GitCommitIcon } from 'svelte-feather-icons';
-	import { filteredProducts, productFeatures } from '$lib/stores';
+  import {GitCommitIcon} from 'svelte-feather-icons';
+  import {filteredProducts, productFeatures} from '$lib/stores';
 
-	import type { HierarchicalCategory } from '$lib/stores';
-	import type { Product, Feature as FeatureType } from '$lib/prisma';
+  import type {HierarchicalCategory} from '$lib/stores';
+  import type {Product, Feature as FeatureType} from '$lib/prisma';
 
-	import Category from '../cells/Category.svelte';
-	import Feature from '../cells/Feature.svelte';
-	import Header from '../cells/Header.svelte';
+  import Category from '../cells/Category.svelte';
+  import Feature from '../cells/Feature.svelte';
+  import Header from '../cells/Header.svelte';
 
-	import ProductFeature from '$lib/components/unified/cells/ProductFeature.svelte';
+  import ProductFeature from '$lib/components/unified/cells/ProductFeature.svelte';
 
-	export let category: HierarchicalCategory;
-	export let feature: FeatureType = undefined;
+  export let category: HierarchicalCategory;
+  export let feature: FeatureType | undefined = undefined;
 
-	const getProductFeature = (p: Product, f: FeatureType) => {
-		return $productFeatures.find((pf) => pf.product_id == p?.id && pf.feature_id == f?.id);
-	};
+  const getProductFeature = (p: Product, f?: FeatureType) => {
+    return $productFeatures.find((pf) => pf.product_id == p?.id && pf.feature_id == f?.id);
+  };
 </script>
 
 <tr class="hover">
-	<!-- First Column: Feature Header -->
-	<Feature {feature} {category} />
-	<!-- Rest Columns: ProductFeature Cells -->
-	{#each $filteredProducts as product}
-		<ProductFeature productFeature={getProductFeature(product, feature)} {product} {feature} />
-	{/each}
+  <!-- First Column: Feature Header -->
+  <Feature {feature} {category}/>
+  <!-- Rest Columns: ProductFeature Cells -->
+  {#each $filteredProducts as product}
+    <ProductFeature productFeature={getProductFeature(product, feature)} {product} {feature}/>
+  {/each}
 </tr>
--- src/routes/api/product_features/[id].json/+server.ts
+++ src/routes/api/product_features/[id].json/+server.ts
@@ -1,52 +1,52 @@
-import { json } from '@sveltejs/kit';
-import { prisma } from "$lib/prisma";
+import {json} from '@sveltejs/kit';
+import {prisma} from "$lib/prisma";
+import {Prisma} from '@prisma/client'
 
 // GET /product_features/:id.json
-export const GET = async ({ params }) => {
-    const id = parseInt(params.id);
-    
-    try {
-        const body = await prisma.productFeature.findUnique({
-            where: { id }
-        })
-        return json(body);
-    } catch ({ code, message }) {
-        return json({ code, message }, {
-            status: 500
-        })
-    }
+export const GET = async ({params}) => {
+  const id = parseInt(params.id);
+
+  try {
+    const body = await prisma.productFeature.findUnique({
+      where: {id}
+    })
+    return json(body);
+  } catch (e) {
+    return json(e, {
+      status: 500
+    })
+  }
 }
 
 // PATCH /product_features/:id.json
-export const PATCH = async ({ params, request }) => {
-    const data = await request.json()
-    const id = parseInt(params.id);
-    
-    try {
-        const body = await prisma.productFeature.update({
-            where: { id },
-            data
-        })
-        return json(body);
-    } catch ({ code, message }) {
-        return json({ code, message }, {
-            status: 500
-        })
-    }
+export const PATCH = async ({params, request}) => {
+  const data = await request.json()
+  const id = parseInt(params.id);
+
+  try {
+    const body = await prisma.productFeature.update({
+      where: {id}, data
+    })
+    return json(body);
+  } catch (e) {
+    return json(e, {
+      status: 500
+    })
+  }
 }
 
 // DELETE /product_features/:id.json
-export const DELETE = async ({ params }) => {
-    const id = parseInt(params.id);
-    
-    try {
-        const body = await prisma.productFeature.delete({
-            where: { id }
-        })
-        return json(body);
-    } catch ({ code, message }) {
-        return json({ code, message }, {
-            status: 500
-        })
-    }
+export const DELETE = async ({params}) => {
+  const id = parseInt(params.id);
+
+  try {
+    const body = await prisma.productFeature.delete({
+      where: {id}
+    })
+    return json(body);
+  } catch (e) {
+    return json(e, {
+      status: 500
+    })
+  }
 }
