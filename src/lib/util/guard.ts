e3347246d1024a707432425b788b0caed0a51f7d
nikolai <nikolai@theministry.co>
2024-06-20 00:57:58 +0200

    type annotation

--- src/lib/util/guard.ts
+++ src/lib/util/guard.ts
@@ -1,6 +1,7 @@
 import { error } from '@sveltejs/kit';
 
-export const guard = async (fetch, base) => {
+
+export const guard = async (fetch: typeof window.fetch, base: Record<string, unknown>) => {
   const res = await fetch('/api/pwd', { method: 'POST' });
   if (res.status == 200) {
     return base;
