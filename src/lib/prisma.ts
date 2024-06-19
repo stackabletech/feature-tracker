428afdf0249087d6d46025873e7d6acdea4b6aaf
nikolai <nikolai@theministry.co>
2024-06-19 16:49:04 +0200

    update prismaclient to latest version from docs

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,31 +1,5 @@
-// According to docs. Does not work:
-
-// import { PrismaClient } from '@prisma/client'
-// declare global { var prisma: PrismaClient | undefined}
-// export const prisma = global.prisma || new PrismaClient()
-
-// ----------------------------------------------------------------------------
-
-import Prisma, * as PrismaAll from '@prisma/client';
-const pc = Prisma?.PrismaClient || PrismaAll?.PrismaClient;
-export const prisma = new pc();
-
-// ----------------------------------------------------------------------------
-
-// import { dev } from '$app/env'
-
-// import Prisma from '@prisma/client'
-// import type { PrismaClient } from '@prisma/client'
-// export let prisma: PrismaClient
-
-// if (dev) {
-//     prisma = new Prisma.PrismaClient();
-// } else {
-//     console.log('not dev')
-//     import('@prisma/client').then(( Prisma ) => {
-//         prisma = new Prisma.PrismaClient();
-//     });
-// }
+import { PrismaClient } from '@prisma/client'
+export const prisma = new PrismaClient()
 
 import type {
   Category,
