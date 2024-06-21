3542fa8d22f0eb0353abe8f7ada8b33eef078ba2
nikolai <nikolai@theministry.co>
2024-06-21 18:48:48 +0200

    fix bool

--- src/lib/components/crud/cells/Boolean.svelte
+++ src/lib/components/crud/cells/Boolean.svelte
@@ -10,7 +10,7 @@
 <td {colspan} {rowspan}>
   <div class="flex flex-row items-center justify-center">
     <div class="tooltip mr-2 h-6 w-6" data-tip={value ? labelTrue : labelFalse}>
-      <input type="checkbox" bind:checked={value} class="checkbox" />
+      <input type="checkbox" bind:checked={value} class="checkbox checkbox-sm" />
     </div>
   </div>
 </td>
